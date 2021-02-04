50c4a43b7ad675313ce37a10a3b36a79aca3628c
Igor Marinho <igorjmv2000@gmail.com>
2021-02-04 13:24:56 -0300

    MainView implemented

--- src/main/java/com/igorjmv2000/gmail/aulajpa/AulaJpa02Application.java
+++ src/main/java/com/igorjmv2000/gmail/aulajpa/AulaJpa02Application.java
@@ -3,11 +3,14 @@ package com.igorjmv2000.gmail.aulajpa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.igorjmv2000.gmail.aulajpa.gui.start.StartGui;
+
 @SpringBootApplication
 public class AulaJpa02Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AulaJpa02Application.class, args);
+		StartGui.initialize(args);
 	}
 
 }
